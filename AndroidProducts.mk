cd2fa69d682e7a17e18d1d9105788d9fdccdf326
LeDrew2017 <ledrew101806@gmail.com>
2023-01-07 21:29:57 -0500

    added bliss

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_beyond1lte.mk
+    $(LOCAL_DIR)/bliss_beyond1lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_beyond1lte-eng \
-    xtended_beyond1lte-user \
-    xtended_beyond1lte-userdebug
+    bliss_beyond1lte-eng \
+    bliss_beyond1lte-user \
+    bliss_beyond1lte-userdebug
